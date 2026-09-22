846b5728c2f1ce674da262fad7d586e671035cba
Harsh Dhirendra Mishra <harshdm1811@gmail.com>
2026-09-23 02:23:04 +0530

    Add HQ9+ [A] from Codeforces

---
+++ 133/A - HQ9+/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin>>s;
+    int c=0;
+    for ( int i = 0 ; i< s.size(); i++)
+    {
+        if ( s[i]=='H' || s[i]== 'Q' || s[i]== '9')
+        {
+            c++;
+        }
+        
+    }
+    if ( c> 0)
+    {
+        cout<<"YES";
+ 
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
