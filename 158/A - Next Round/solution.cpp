2090e782fe495aed331a6fe4cb04dfe314739c5f
Harsh Dhirendra Mishra <harshdm1811@gmail.com>
2026-09-23 02:23:14 +0530

    Add Next Round [A] from Codeforces

---
+++ 158/A - Next Round/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+    int n, k;
+    cin >> n >> k;
+ 
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+ 
+    int c = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > 0 && arr[i] >= arr[k-1])
+            c++;
+    }
+ 
+    cout << c;
+}
