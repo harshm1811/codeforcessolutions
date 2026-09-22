4027ceec6b92292ffc7514391ad79af8ff205163
Harsh Dhirendra Mishra <harshdm1811@gmail.com>
2026-09-23 02:29:02 +0530

    Add Drinks [B] from Codeforces

---
+++ 200/B - Drinks/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    int a[n];
+    double sum = 0;
+    for(int i = 0 ; i<n ; i++)
+    {
+        cin>>a[i];
+        sum+=a[i];
+ 
+    }
+    cout<<fixed<<setprecision(12)<<sum/n<<"
+";
+ 
+    return 0;
+}
