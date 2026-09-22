622e9d9665d2763457bf1765f1e6c4c2ccd3efb0
Harsh Dhirendra Mishra <harshdm1811@gmail.com>
2026-09-23 02:25:22 +0530

    Add The 67th Integer Problem [A] from Codeforces

---
+++ 2218/A - The 67th Integer Problem/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    int x;
+    while(t--)
+ 
+    { cin>>x;
+      if ( x < 67)
+       {
+       
+       cout<<x+1<<"
+";}
+       else
+       cout<<67<<"
+";
+    }
+  
+ 
+}
