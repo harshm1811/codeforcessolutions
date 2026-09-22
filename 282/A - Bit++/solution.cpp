2210e7cbf0f68ada4eefe0764be213ec932652fd
Harsh Dhirendra Mishra <harshdm1811@gmail.com>
2026-09-23 02:23:29 +0530

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    int x = 0;
+    string s;
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+ 
+        if (s[1] == '+')  // ++X or X++
+            x++;
+        else              // --X or X--
+            x--;
+    }
+ 
+    cout << x << endl;
+    return 0;
+}
