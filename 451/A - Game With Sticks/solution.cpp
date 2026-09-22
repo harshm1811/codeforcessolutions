bff875a0a099897b0778d9957de75f0811b5728a
Harsh Dhirendra Mishra <harshdm1811@gmail.com>
2026-09-23 02:31:45 +0530

    Add Game With Sticks [A] from Codeforces

---
+++ 451/A - Game With Sticks/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define nl '
+'
+ 
+void solve() {
+    int n,m;
+    cin>>n>>m;
+    if(min(n,m)%2==0) cout<<"Malvika";
+    else cout<<"Akshat";
+ 
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+        solve();
+    
+    return 0;
+}
