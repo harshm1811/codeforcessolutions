dfda90856e7925c1f542b32fbd40efc31b80c2d7
Harsh Dhirendra Mishra <harshdm1811@gmail.com>
2026-09-23 02:22:33 +0530

    Add Hulk [A] from Codeforces

---
+++ 705/A - Hulk/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+ 
+  if ( n%2==0) 
+    {for ( int j = 0 ; j< n/2 - 1 ; j++)
+    {   
+        cout<<" I hate that I love that";
+      
+}
+     cout<<" I hate that I love it";
+    }
+    else
+    {
+        for (int j = 0 ;  j < (n-1)/2 ; j++)
+        
+        cout<<" I hate that I love that" ;
+        cout <<" I hate it";}
+ 
+ 
+}
